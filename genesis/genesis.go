74f8f09d78a354a2726bc4b3c6fcf9cca266ce91
Justin Casso <justincasso1@gmail.com>
2021-02-19 13:54:33 -0800

    Add initial CI config schema; sorting

---
+++ genesis/genesis.go
@@ -0,0 +1,98 @@
+package genesis
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
+)
+
+// This is the pattern for a CI system:
+//
+// Nodes are updated to possess a completion state: success/failure
+// Each represents a container that needs to run during the build, defined by a DAG-defined dependency tree.
+// Every cycle, the DAG is traversed and the state of each node's container is assessed.
+// New containers that meet initialization conditions are then dispatched.
+// If the containers exited for any other reason than 0, then the node status will reflect this with failure,
+// and the dependent nodes will not dispatch, and will be given completion state: skipped.
+// When all node completion states are no longer pending, the build completes.
+
+// This is the pattern for a component testing system:
+//
+// A system evaluation cycle is repeated with frequency (e.g. 1 second). This
+
+// Pipeline represents one CI pipeline, which has many steps
+type Pipeline struct {
+	Name  string `yaml:"name"`
+	Steps []Step `yaml:"steps"`
+}
+
+// Step represents A CI pipeline step
+type Step struct {
+	Name        string   `yaml:"name"`
+	Image       string   `yaml:"image"`
+	Command     string   `yaml:"command,omitempty"`
+	DependsOn   []string `yaml:"depends_on,flow,omitempty"`
+	Environment []string `yaml:"environment,flow,omitempty"`
+}
+
+// CreateNewContainer Creates a new container
+func CreateNewContainer(ctx context.Context, cli client.Client, image string, c chan string, wg *sync.WaitGroup) {
+	hostBinding := nat.PortBinding{
+		HostIP:   "0.0.0.0",
+		HostPort: "8000",
+	}
+	containerPort, err := nat.NewPort("tcp", "80")
+	if err != nil {
+		panic("Unable to get the port")
+	}
+
+	portBinding := nat.PortMap{containerPort: []nat.PortBinding{hostBinding}}
+	cont, err := cli.ContainerCreate(
+		ctx,
+		&container.Config{
+			Image: image,
+		},
+		&container.HostConfig{
+			PortBindings: portBinding,
+		}, nil, "")
+	if err != nil {
+		panic(err)
+	}
+
+	cli.ContainerStart(ctx, cont.ID, types.ContainerStartOptions{})
+	wg.Done()
+	c <- cont.ID
+}
+
+// DeleteContainer adsa
+func DeleteContainer(ctx context.Context, cli client.Client, id string) {
+	var info types.ContainerJSON
+	var removeOptions = types.ContainerRemoveOptions{
+		RemoveVolumes: true,
+		RemoveLinks:   false,
+		Force:         true,
+	}
+
+	info, err := cli.ContainerInspect(ctx, id)
+	fmt.Printf("%+v\n", info.ContainerJSONBase)
+
+	err = cli.ContainerStop(ctx, id, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	err = cli.ContainerRemove(ctx, id, removeOptions)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// ContainerList
+func ContainerList(ctx context.Context, cli client.Client, id string) {
+
+}
---
+++ main.go
@@ -0,0 +1,226 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"sync"
+
+	"github.com/docker/docker/client"
+	"github.com/jrcasso/genesis/genesis"
+	"github.com/jrcasso/gograph"
+	"gopkg.in/yaml.v2"
+)
+
+func validateConfiguration(config []byte) bool {
+	return true
+}
+
+func readFile(path string) []byte {
+	// Load YAML file
+	configBytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	fmt.Print(string(configBytes))
+	return configBytes
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func unmarshalConfigYaml(config []byte) genesis.Pipeline {
+	t := genesis.Pipeline{}
+	err := yaml.Unmarshal(config, &t)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	return t
+}
+
+func convertConfigToGraph(config genesis.Pipeline) gograph.DirectedGraph {
+	// We're going to instantiate an empty directed graph, then add directed nodes to this graph
+	// But the nodes will not have direction, yet!
+	var graph = gograph.DirectedGraph{DirectedNodes: []*gograph.DirectedNode{}, RootDirectedNode: nil}
+	var rootOverride = false
+
+	for _, s := range config.Steps {
+		if s.Name == "root" {
+			rootOverride = true
+		}
+		var newNode = &gograph.DirectedNode{
+			Parents:  []*gograph.DirectedNode{},
+			Children: []*gograph.DirectedNode{},
+			Values: map[string]string{
+				"name":    s.Name,
+				"image":   s.Image,
+				"command": s.Command,
+				"state":   "pending",
+			},
+			ID: gograph.CreateDirectedNodeID(),
+		}
+		graph.DirectedNodes = append(graph.DirectedNodes, newNode)
+		// One optimization is to try to set directed edge from a node to its dependency *here*. If it fails, put the step
+		// in an array for processing in a block after this routine. In this follow-up block, *all* node dependencies are guaranteed
+		// to exist (by virtue of this node instantiation), but we're able to take advantage of on-the-fly edge creation
+		// in this loop! The next bock would simply fill in the edges where on-the-fly couldn't create them because a node didn't exist
+	}
+
+	// Add default root node if one was not specified
+	if !rootOverride {
+		var newRootNode = &gograph.DirectedNode{
+			Parents:  []*gograph.DirectedNode{},
+			Children: []*gograph.DirectedNode{},
+			Values: map[string]string{
+				"name":  "root",
+				"image": "sverrirab/sleep",
+				"state": "pending",
+			},
+			ID: gograph.CreateDirectedNodeID(),
+		}
+		graph.DirectedNodes = append(graph.DirectedNodes, newRootNode)
+		graph.RootDirectedNode = newRootNode
+	}
+
+	// Make the root node the parent of all other nodes, and all other nodes children of the root
+	// node. We can't start CI without bootstrapping the first step.
+	for _, node := range graph.DirectedNodes {
+		if node.ID != graph.RootDirectedNode.ID {
+			graph, _, _ = gograph.CreateDirectedEdge(graph, graph.RootDirectedNode, node)
+		}
+	}
+
+	// Now let's process the steps again, this time assigning directed edges. We couldn't do this
+	// before because we had no guarantee that a given node dependency had been initialized.
+	for _, s := range config.Steps {
+		for _, dependency := range s.DependsOn {
+			var childNode = findNodesByValues(graph, map[string]string{"name": s.Name})
+			var parentNode = findNodesByValues(graph, map[string]string{"name": dependency})
+			if childNode == nil {
+				panic(fmt.Sprintf("Node not found: '%+v'. Did you spell step names and dependencies correctly?", s.Name))
+			}
+			if parentNode == nil {
+				panic(fmt.Sprintf("Node not found: '%+v'. Did you spell step names and dependencies correctly?", dependency))
+			}
+			if len(parentNode) > 1 || len(childNode) > 1 {
+				panic("Multiple nodes with the same name found!")
+			}
+			graph, _, _ = gograph.CreateDirectedEdge(graph, parentNode[0], childNode[0])
+		}
+	}
+
+	// In graph theory, a topological sort or topological ordering of a directed acyclic graph (DAG)
+	// is a linear ordering of its nodes in which each node comes before all nodes to which it has
+	// outbound edges. Every DAG has one or more topological sorts. We attempt to sort the DAG here
+	// to panic if it is not possible.
+
+	var sortedNodes = topologicalSort(graph)
+	var sortedByName = []string{}
+	for _, node := range sortedNodes {
+		sortedByName = append(sortedByName, node.Values["name"])
+	}
+	fmt.Println(sortedNodes)
+	fmt.Println(sortedByName)
+
+	return graph
+}
+
+func DeleteDirectedEdge(graph gograph.DirectedGraph, parent *gograph.DirectedNode, child *gograph.DirectedNode) {
+	for index, childNode := range parent.Children {
+		if childNode.ID == child.ID {
+			parent.Children = append(parent.Children[:index], parent.Children[index+1:]...)
+			break
+		}
+	}
+	for index, parentNode := range child.Parents {
+		if parentNode.ID == parent.ID {
+			child.Parents = append(child.Parents[:index], child.Parents[:index+1]...)
+			break
+		}
+	}
+}
+
+//  topologicalSort implements Khan's algorithm
+func topologicalSort(graph gograph.DirectedGraph) []*gograph.DirectedNode {
+	var sorted []*gograph.DirectedNode
+	var childlessNodes []*gograph.DirectedNode
+	for _, node := range graph.DirectedNodes {
+		if len(node.Children) == 0 {
+			childlessNodes = append(childlessNodes, node)
+		}
+	}
+
+	for len(childlessNodes) > 0 {
+		var nextNode = childlessNodes[0]
+		childlessNodes = childlessNodes[1:]
+		sorted = append(sorted, nextNode)
+		for _, parent := range nextNode.Parents {
+			// Remove edge from parent to this node
+			DeleteDirectedEdge(graph, parent, nextNode)
+			if len(parent.Children) == 0 {
+				childlessNodes = append(childlessNodes, parent)
+			}
+		}
+	}
+
+	return sorted
+}
+
+func findNodesByValues(graph gograph.DirectedGraph, values map[string]string) []*gograph.DirectedNode {
+	var isMatch bool
+	var results []*gograph.DirectedNode
+
+	for _, node := range graph.DirectedNodes {
+		isMatch = true
+		for key, value := range values {
+			if node.Values[key] != value {
+				isMatch = false
+			}
+		}
+		if isMatch {
+			results = append(results, node)
+		}
+	}
+	return results
+}
+
+func main() {
+	// Iniitialize
+	var config = unmarshalConfigYaml(readFile("./fixtures/.genesis.yml"))
+	var graph = convertConfigToGraph(config)
+
+	fmt.Printf("%+v\n", config)
+	// // The daemon process is the root node
+	// graph, rootNode = gograph.CreateDirectedNode(graph, map[string]string{"foo": "bar"}, []*gograph.DirectedNode{}, []*gograph.DirectedNode{})
+	// ptrNode = rootNode
+
+	fmt.Printf("%+v\n", graph)
+}
+
+func dispatchContainers() {
+	var c chan string = make(chan string)
+	var wg sync.WaitGroup
+	ctx := context.Background()
+
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		fmt.Println("Unable to create docker client")
+		panic(err)
+	}
+
+	for i := 0; i < 1; i++ {
+		wg.Add(1)
+		go genesis.CreateNewContainer(ctx, *cli, "sverrirab/sleep", c, &wg)
+	}
+
+	var id = <-c
+	fmt.Println("Waiting for containers to finish")
+	wg.Wait()
+	fmt.Println("Stopping containers")
+	fmt.Println(id)
+	genesis.DeleteContainer(ctx, *cli, id)
+}
